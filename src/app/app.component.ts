5cfea8568999bf405de348c119c579d6f21ae44c
Deletking <deletkingdelet@gmail.com>
2023-10-16 14:56:58 -0300

    feat: testando o signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'signals';
+
+  protected test = signal(1);
+  protected computedSignal = computed(() => `${this.test()} computed!`);
+
+  protected showCount = signal(true);
+
+  init() {
+    // this.test.set(10);
+    // this.test.update((value) => {
+    //   return { ...value, idade: 30 };
+    // });
+    // this.test.mutate((value) => (value.idade = 25));
+    this.test.update((value) => value + 1);
+  }
 }
